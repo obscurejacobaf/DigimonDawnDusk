a5b699eae1c24271a296f9ad316df7168138bf22
obscurejacobaf <thejacobforte@gmail.com>
2026-04-26 23:07:48 -0400

    Optimized the tree builder.

--- DigimonDawnDusk/Components/EvolutionPath.cs
+++ DigimonDawnDusk/Components/EvolutionPath.cs
@@ -4,8 +4,8 @@ namespace DigimonDawnDusk.Components;
 
 public class EvolutionPath
 {
-	public Move? MoveGained { get; set; }
+	public List<Move> MovesGained { get; set; } = [];
 	public List<Digimon> Nodes { get; set; } = [];
 	public override string ToString() =>
-		$"{(MoveGained == null ? "" : $"{MoveGained.Name}: ")}{string.Join(" -> ", Nodes.Select(x => x.Name))}";
+		$"{(MovesGained.Count == 0 ? "" : $"{string.Join(", ", MovesGained)}: ")}{string.Join(" -> ", Nodes.Select(x => x.Name))}";
 }
