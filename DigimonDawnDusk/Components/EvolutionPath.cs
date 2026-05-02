12f45d96519211d51af8c321ab4cca72aea9a151
obscurejacobaf <thejacobforte@gmail.com>
2026-05-02 19:15:48 -0400

    Switched the search tree to get the shortest possible path

--- DigimonDawnDusk/Components/EvolutionPath.cs
+++ DigimonDawnDusk/Components/EvolutionPath.cs
@@ -2,15 +2,18 @@
 
 namespace DigimonDawnDusk.Components;
 
-public class EvolutionPath
+public class EvolutionPath(List<EvolutionNode> nodes, HashSet<Digimon> visited, HashSet<Move> movesRequired)
 {
-	public List<Move> MovesGained { get; set; } = [];
-	public List<EvolutionNode> Nodes { get; set; } = [];
+	public bool SeekTarget => RequiredMoves.Count == 0;
+
+	public HashSet<Move> RequiredMoves { get; set; } = movesRequired;
+	public HashSet<Digimon> Visited { get; set; } = visited;
+	public List<EvolutionNode> Nodes { get; set; } = nodes;
 }
 
-public record EvolutionNode(Digimon Digimon, Evolution? Evolution = null) : IDescriptor
+public record EvolutionNode(Digimon Digimon, HashSet<Move> LearnedMoves, Evolution? Evolution = null) : IDescriptor
 {
 	public string Name => Digimon.ToString();
-
+	public bool HasLearnedMoves => LearnedMoves?.Count > 0;
 	public string Descriptor => Evolution?.Descriptor ?? "";
 }
